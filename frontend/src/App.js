088a0f226d209e9703de84c76f0936c496746f36
Saikat Mal <saikatmal306@gmail.com>
2026-04-21 01:43:18 +0530

    Day 17: React login, Axios, JWT auth, protected route, show user in UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,81 @@
 import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault(); // ❗ stops  page   reload
-    console.log(name);
-  };
+  const handleLogin = async () => {
+  try {
+    const res = await axios.post(
+      "http://localhost:5000/api/users/login",
+      {
+        email,
+        password,
+      }
+    );
+
+    console.log(res.data);
+
+    // 🔥 STORE TOKEN
+    localStorage.setItem("token", res.data.token);
+
+  } catch (error) {
+    console.log(error.response?.data || error.message);
+  }
+};
+
+const getProfile = async () => {
+  try {
+    const token = localStorage.getItem("token");
+
+    const res = await axios.get(
+      "http://localhost:5000/api/users/profile",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    setUser(res.data.data); // 🔥 store in state
+  } catch (error) {
+    console.log(error.response?.data || error.message);
+  }
+};
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div>
+      <h1>Login</h1>
+
       <input
-        type="text"
-        placeholder="Enter name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+        type="email"
+        placeholder="Enter email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
-      <button type="submit">Submit</button>
-    </form>
+
+      <input
+        type="password"
+        placeholder="Enter password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>            
+      <button onClick={getProfile}>Get Profile</button>
+
+      {user && (
+  <div>
+    <h2>{user.name}</h2>
+    <p>{user.email}</p>
+  </div>
+)}
+    </div>
   );
 }
+
+
 
 export default App;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const cors = require("cors");
 
 const express = require("express");
 const connectDB = require("./config/db");
@@ -8,9 +9,10 @@ const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Logger middleware
